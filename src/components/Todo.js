f7339d3cdfa5307c104a57372ea760a7579aaf0b
Steve Molele <stevemolele@yahoo.com>
2022-04-11 16:55:59 +0200

    added logic to open and close modal

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,27 @@
+import { useState } from "react";
+import Backdrop from "./Backdrop";
+import Modal from "./Modal";
+
 function Todo(props) {
-  function deleteHandler() {}
+  const [isModalOpen, openModal] = useState(false);
+  function deleteBtnHandler() {
+    openModal(true);
+  }
+
+  function closeModal() {
+    openModal(false);
+  }
+
   return (
     <div className="card">
       <h2>{props.todoTitle}</h2>
       <div className="actions">
-        <button className="btn" onClick={deleteHandler}>
+        <button className="btn" onClick={deleteBtnHandler}>
           Delete
         </button>
       </div>
+      {isModalOpen && <Modal onCancel={closeModal} onConfirm={closeModal} />}
+      {isModalOpen ? <Backdrop onClick={closeModal} /> : null}
     </div>
   );
 }
